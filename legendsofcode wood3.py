489fd04d5e96a419948bd2e3d42b6e1ae3884a2d
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2021-07-19 21:24:46 +0900

    instance id dictionary

--- legendsofcode wood3.py
+++ legendsofcode wood3.py
@@ -7,8 +7,10 @@ import math
 # constant
 DRAFT = True
 BATTLE = False
+turn = 0
 # game loop
 while True:
+    print("turn ", turn, file=sys.stderr)
     player = []
     for i in range(2):
         player_health, player_mana, player_deck, player_rune, player_draw = [int(j) for j in input().split()]
@@ -41,20 +43,23 @@ while True:
         print(card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw, file=sys.stderr)
         if location == 0: # Draft phase
             # NOTE: key is changed insatance id to card_number. which is best?
-            player_hand_map[card_number] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+            if DRAFT == True:
+                player_hand_map[card_number] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+            elif DRAFT == False and BATTLE == False:
+                player_hand_map[card_number] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+                BATTLE = True
+            else:
+                player_hand_map[instance_id] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+
         if location == 1:
-            player_board_map[card_number] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
-        if location == 2:
-            opponent_board_map[card_number] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+            player_board_map[instance_id] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+        if location == -1:
+            opponent_board_map[instance_id] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
 
 
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-    if player[0][2] == 29:
-        DRAFT = False
-        BATTLE = True
 
     # Draft mode
     if DRAFT:
@@ -67,7 +72,7 @@ while True:
         # NOTE:changed card count forin to plyaerhandmap key
         # draft phase instance id is all -1
         for i, k in enumerate(player_hand_map.keys()):
-            # BUG:key is not 0, 1, 2,
+            # NOTE:key is not 0, 1, 2,
             attack = player_hand_map[k][5]
             if attack_max <= attack:
                 attack_max = attack
@@ -79,4 +84,17 @@ while True:
     # Battle mode
     if BATTLE:
         print("Battle phase", file=sys.stderr)
+        print(player_hand_map, file=sys.stderr)
+        print(player_board_map, file=sys.stderr)
+        print(opponent_board_map, file=sys.stderr)
+        # summon
+        # attack creature
+        # attack opponent(-1)
+
         print("PASS")
+
+    if player[0][2] == 29:
+        DRAFT = False
+        BATTLE = True
+
+    turn += 1
