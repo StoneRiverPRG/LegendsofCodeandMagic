75ba67ffbe6869f4ead2baebc0f3a7c64e3d45c0
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2021-07-19 15:25:57 +0900

    変数確認

--- legendsofcode wood3.py
+++ legendsofcode wood3.py
@@ -4,14 +4,26 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-
+# constant
+DRAFT = True
+BATTLE = False
 # game loop
 while True:
+    player = []
     for i in range(2):
         player_health, player_mana, player_deck, player_rune, player_draw = [int(j) for j in input().split()]
+        player.append([player_health, player_mana, player_deck, player_rune, player_draw])
+        print(player_health, player_mana, player_deck, player_rune, player_draw, file=sys.stderr)
+
     opponent_hand, opponent_actions = [int(i) for i in input().split()]
+    print(opponent_hand, opponent_actions, file=sys.stderr)
     for i in range(opponent_actions):
         card_number_and_action = input()
+        print(card_number_and_action, file=sys.stderr)
+
+    player_hand_map = {}
+    player_board_map = {}
+    opponent_board_map = {}
     card_count = int(input())
     for i in range(card_count):
         inputs = input().split()
@@ -26,8 +38,29 @@ while True:
         my_health_change = int(inputs[8])
         opponent_health_change = int(inputs[9])
         card_draw = int(inputs[10])
+        print(card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw, file=sys.stderr)
+        if location == 0:
+            player_hand_map[instance_id] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+        if location == 1:
+            player_board_map[instance_id] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+        if location == 2:
+            opponent_board_map[instance_id] = (card_number, instance_id, location, card_type, cost, attack, defense, abilities, my_health_change, opponent_health_change, card_draw)
+
+
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-    print("PASS")
+    if player[0][2] == 29:
+        DRAFT = False
+        BATTLE = True
+
+    # Draft mode
+    if DRAFT:
+        print("Draft mode", file=sys.stderr)
+        print("PASS")
+
+    # Battle mode
+    if BATTLE:
+        print("Battle mode", file=sys.stderr)
+        print("PASS")
